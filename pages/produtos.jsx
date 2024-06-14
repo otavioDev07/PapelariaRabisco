5ca16dfef3e496ce1edeb9a6f6badd62da302923
Otávio <netootavio223@outlook.com>
2024-06-14 15:04:42 -0300

    chamada realizada com axius

--- pages/produtos.jsx
+++ pages/produtos.jsx
@@ -3,15 +3,16 @@ import PageTitle from '../components/PageTitle'
 import CardList from '@/components/CardList'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React, { useEffect, useState } from 'react'
-import { getProdutos } from '@/services/api'
+import axios from 'axios'
 export default function produtos(){
     const [produtos, setProdutos] = useState([]);
       
         useEffect(() => {
           const fetchProdutos = async () => {
             try {
-              const data = await getProdutos();
-              setProdutos(data);
+              const data = axios.get('http://localhost:80/produto').then((response) => {
+                setProdutos(response.data);
+              })
             } catch (error) {
               console.error("Erro ao buscar produtos:", error);
             }
